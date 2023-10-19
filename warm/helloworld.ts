bd7b978cfe38a05e4d32cf172ae2c67d2f310653
yukiice <58852690+yukiice@users.noreply.github.com>
2023-10-19 09:55:55 +0800

    init workspace & over challenge 1

---
+++ warm/helloworld.ts
@@ -0,0 +1,9 @@
+type HelloWorld = string // expected to be a string
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect, NotAny } from '@type-challenges/utils'
+
+type cases = [
+  Expect<NotAny<HelloWorld>>,
+  Expect<Equal<HelloWorld, string>>,
+]
